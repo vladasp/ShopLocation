40acc069d2680656ab949b0f5eec9bf9e774554a
vladasp <vladalogin@ukr.net>
2016-11-12 12:13:28 +0200

    Added project

---
+++ ShopLocation/ShopLocation/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ShopLocation {
+    [Activity(Label = "ShopLocation", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
